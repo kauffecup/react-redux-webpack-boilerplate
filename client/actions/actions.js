77e4837c1859cd92b163d59013b55e4f415b16f6
Jonathan Kaufman <jondakauf@gmail.com>
2016-02-03 16:51:34 -0500

    remove dedicated constants file - have constants generated and exported via the actions file that uses them

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,17 +1,21 @@
-import Constants  from '../constants/Constants';
 import {exampleRequest} from '../requester';
 
+export const EXAMPLE_CONSTANT = 'EXAMPLE_CONSTANT';
+export const EXAMPLE_ASYNC_BEGIN = 'EXAMPLE_ASYNC_BEGIN';
+export const EXAMPLE_ASYNC_SUCCESS = 'EXAMPLE_ASYNC_SUCCESS';
+export const EXAMPLE_ASYNC_FAILURE = 'EXAMPLE_ASYNC_FAILURE';
+
 export function exampleSimpleAction() {
-  return { type: Constants.EXAMPLE_CONSTANT };
+  return { type: EXAMPLE_CONSTANT };
 }
 
 export function exampleAsyncAction(url) {
   return dispatch => {
-    dispatch({ type: Constants.EXAMPLE_ASYNC_BEGIN});
+    dispatch({ type: EXAMPLE_ASYNC_BEGIN});
     exampleRequest(url).then(data => {
-      dispatch({ type: Constants.EXAMPLE_ASYNC_SUCCESS, data: data });
+      dispatch({ type: EXAMPLE_ASYNC_SUCCESS, data: data });
     }).catch(e => {
-      dispatch({ type: Constants.EXAMPLE_ASYNC_FAILURE, error: e });
+      dispatch({ type: EXAMPLE_ASYNC_FAILURE, error: e });
     });
   };
 }
--- client/constants/Constants.js
+++
@@ -1,8 +0,0 @@
-import keyMirror from 'keymirror';
-
-module.exports = keyMirror({
-  EXAMPLE_CONSTANT: null,
-  EXAMPLE_ASYNC_BEGIN: null,
-  EXAMPLE_ASYNC_SUCCESS: null,
-  EXAMPLE_ASYNC_FAILURE: null,
-});
